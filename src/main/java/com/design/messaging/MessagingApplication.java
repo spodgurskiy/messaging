51062d2a42b6525488061d70d2b0f9a032986c74
spodgurskiy <spodgurskiy@freedompop.com>
2018-08-22 13:17:37 -0700

    Add spring modularization support

--- src/main/java/com/design/messaging/MessagingApplication.java
+++ src/main/java/com/design/messaging/MessagingApplication.java
@@ -1,13 +1,17 @@
 package com.design.messaging;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.design.messaging.requestreply.RequestReplyConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory;
 import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
@@ -15,8 +19,11 @@ import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import java.util.concurrent.Executors;
 
 @EnableSqs
+@Configuration
 @EnableScheduling
-@SpringBootApplication
+@EnableAutoConfiguration
+@Import(RequestReplyConfig.class)
+@ComponentScan(basePackages = "com.design.messaging.reply")
 public class MessagingApplication {
 
     @Bean
---
+++ src/main/java/com/design/messaging/requestreply/RequestReplyConfig.java
@@ -0,0 +1,9 @@
+package com.design.messaging.requestreply;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class RequestReplyConfig {
+}
